753d24a51d3e640cdd072337eeb54697ed7c176d
HardikaB <hardi.2607@gmail.com>
2018-08-14 16:57:01 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,6 +1,38 @@
+# %load q01_load_data/build.py
 # Default imports
+import os
 from sklearn.datasets import load_files
 from sklearn.model_selection import train_test_split
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore') 
 
 path = 'data/20news-bydate-train/'
 # Your solution here:
+def q01_load_data(path,seed=9):
+    CatD=['alt.atheism','soc.religion.christian','comp.graphics','sci.med']
+    
+    '''trainX =np.array([])
+    labels = []
+    seed=9
+    for cat in CatD:
+        files = os.listdir(path + cat + '/');
+        tmpX, tmpY = np.array([]), []
+        for file in files:
+            f = open(path + cat + '/' + file, 'r',encoding = 'unicode_escape')
+            data = f.read()
+            tmpX = np.append(tmpX,data)
+            tmpY = tmpY + [cat]
+            f.close()
+        trainX = np.append(trainX, tmpX)
+        labels = labels + tmpY'''
+        
+    data=load_files(path, categories=CatD, load_content=True, encoding='unicode_escape', decode_error='strict', random_state=9)
+    dict1={0:'alt.atheism',1: 'comp.graphics', 2:'sci.med', 3:'soc.religion.christian'}
+    labels = list()
+    for i in data['target']:
+        labels.append((dict1[i]))
+    X_train, X_test, y_train, y_test = train_test_split(data['data'], labels, train_size = 0.8)
+    return (data),(X_train), (X_test), np.array(y_train), np.array(y_test)
+
+
--- q02_tokenize/build.py
+++ q02_tokenize/build.py
@@ -1,3 +1,4 @@
+# %load q02_tokenize/build.py
 # Default imports
 
 from nltk.tokenize import TreebankWordTokenizer
@@ -7,3 +8,17 @@ import pandas as pd
 from greyatomlib.nlp_day_01_project.q01_load_data.build import q01_load_data
 
 # Write your solution here:
+def q02_tokenize(path):
+    data,X_train,X_test,y_train,y_test=q01_load_data(path)
+    X_train=pd.Series(X_train)
+    X_train=X_train.apply(lambda x:x.lower())
+    tree=TreebankWordTokenizer()
+    Ftokens=pd.Series()
+    Ltokens=[]
+    for i in X_train:
+        tokens=tree.tokenize(str(i))
+        Ltokens.append(tokens)
+        
+    Ftokens=pd.Series(Ltokens)
+    return Ftokens  
+
